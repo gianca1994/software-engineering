8cf642cb8c5e8c4db0b432fe3fff18c19e07c33a
Giancarlo <gianca9405@gmail.com>
2021-04-12 16:01:54 -0300

    Update

--- src/db/update_tables.py
+++ src/db/update_tables.py
@@ -1,8 +1,9 @@
-from src.db.connect_db import connect
+"""from src.db.connect_db import connect
 
 
 def db_update():
     conn = connect()
     cursorObj = conn.cursor()
-    cursorObj.execute('UPDATE employees SET name = "Rogers" where id = 2')
+    cursorObj.execute('UPDATE dirs)
     conn.commit()
+"""
